9d171a9d0773524f6faf9318ae393a271a0324fc
kranthu <g.kranthi2507@gmail.com>
2024-07-30 02:53:24 +0530

    test summaries with db

--- project/tests/conftest.py
+++ project/tests/conftest.py
@@ -5,6 +5,8 @@ Define fixtures
 import os
 
 import pytest
+from tortoise.contrib.fastapi import RegisterTortoise
+
 from starlette.testclient import TestClient
 
 from app.main import create_application, lifespan
@@ -26,6 +28,27 @@ def test_app():
     # setup
     app = create_application(lifespan)
     app.dependency_overrides[get_settings] = get_settings_override
+    with TestClient(app) as test_client:
+
+        # testing
+        yield test_client
+
+    # tear down
+
+
+@pytest.fixture(scope="module")
+def test_app_with_db():
+    """Fixture for testing app with database"""
+    app = create_application(lifespan)
+    app.dependency_overrides[get_settings] = get_settings_override
+    RegisterTortoise(
+        app,
+        db_url=os.environ.get("DATABASE_TEST_URL"),
+        modules={"models": ["app.models.tortoise"]},
+        generate_schemas=True,
+        add_exception_handlers=True,
+    )
+
     with TestClient(app) as test_client:
 
         # testing
